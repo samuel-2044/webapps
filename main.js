907839e6215aaf2de4cc1df5759fa9eea0c457c6
makos-9 <makossammy@gmail.com>
2026-05-14 12:59:47 +0300

    css formating

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 // Form submission (basic)
 const contactForm = document.querySelector('form');
 if (contactForm) {
-    contactForm.addEventListener('submit', (e) => {
-        e.preventDefault();
-        alert('Thank you for your message! I will get back to you soon.');
-        contactForm.reset();
-    });
+   contactForm.addEventListener('submit', (e) => {
+      e.preventDefault();
+      alert('Thank you for your message! I will get back to you soon.');
+      contactForm.reset();
+   });
 }
